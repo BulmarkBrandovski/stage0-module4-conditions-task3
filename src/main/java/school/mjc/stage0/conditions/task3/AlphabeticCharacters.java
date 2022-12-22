1369a9cf8cb0fbe523261ce5c5e55d777ec2844d
bulmark <shamahamaa@gmail.com>
2022-12-22 21:21:15 +0300

    complete tasks

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        String ch = Character.toString(character);
+        if (!ch.matches("[A-Za-z]")) {
+            System.out.println("wrong alphabet!");
+        } else if (ch.matches("[aeyuioAEYUIO]")) {
+            System.out.println("Vowel");
+        } else {
+            System.out.println("Consonant");
+        }
     }
 }
